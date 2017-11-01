826d0cda3ef5b3faa741994e77b202b49f2d068a
leolee2017 <leolee85267@gmail.com>
2017-11-01 18:58:03 +0800

    remove the none necessary part

--- main.js
+++ main.js
@@ -1,15 +1,5 @@
 $(document).ready(() => {
-  $('#ajax_btn').click(() => {
-    $.ajax({
-      type: 'GET',
-      url: './ajax_data',
-      success(response) {
-        $('#ajax_content').text(response);
-      }   
-    });
-  });
-   
-  $('.headingSlide').animate({width: '137px'}, 650, 'easeInOutSine' , () => {
+ $('.headingSlide').animate({width: '137px'}, 650, 'easeInOutSine' , () => {
      $('.headingSlide h1').animate({width: '137px'}, 650, 'easeInOutSine');
    });
 
